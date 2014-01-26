0a55ab5f078c4363649194a75d341f3bb76eadc1
Ben Cox <ben@benjojo.co.uk>
2014-01-26 13:18:46 +0000

    ??? added some cool ish tests, not sure why some of them don't pass

--- test/test_spec.js
+++ test/test_spec.js
@@ -8,4 +8,23 @@ describe("col", function () {
     expect(!x).toBe(false);
   });
 
+  for (var r = 0; r < 255; r = r + 5) {
+    for (var g = 0; g < 255; g = g + 5) {
+      it("Should actually be a thing", function () {
+        var x = new labcol();
+        var a = x.RGBtoLab(r,g,0);
+        var b = x.LabtoRGB(a.l,a.a,a.b);
+
+        expect(r == Math.ceil(b.r)).toBe(true);
+      });
+      // it("Should actually be a thing", function () {
+      //   var x = new labcol();
+      //   var a = x.RGBtoLab(r,g,0);
+      //   var b = x.LabtoRGB(a.l,a.a,a.b);
+
+      //   expect(g == Math.floor(b.g)).toBe(true);
+        
+      // });
+    }
+  }
 });
