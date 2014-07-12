5d60ba989af3a7a06dd85ceff1a56e1c9c6b62ed
Lex Robinson <lexi@lexi.org.uk>
2014-07-12 17:44:26 +0100

    Run JS Beautify on the test spec

--- test/test_spec.js
+++ test/test_spec.js
@@ -1,19 +1,19 @@
 var fs = require('fs');
-eval(fs.readFileSync('../col.js')+'');
+eval(fs.readFileSync('../col.js') + '');
 
 var x = new labcol();
 
-describe("col", function () {
-  it("Should actually be a thing", function () {
+describe("col", function() {
+  it("Should actually be a thing", function() {
     expect(!x).toBe(false);
   });
 
   for (var r = 0; r < 255; r = r + 5) {
     for (var g = 0; g < 255; g = g + 5) {
-      it("Should actually be a thing", function () {
+      it("Should actually be a thing", function() {
         var x = new labcol();
-        var a = x.RGBtoLab(r,g,0);
-        var b = x.LabtoRGB(a.l,a.a,a.b);
+        var a = x.RGBtoLab(r, g, 0);
+        var b = x.LabtoRGB(a.l, a.a, a.b);
 
         expect(r == Math.ceil(b.r)).toBe(true);
       });
@@ -23,7 +23,7 @@ describe("col", function () {
       //   var b = x.LabtoRGB(a.l,a.a,a.b);
 
       //   expect(g == Math.floor(b.g)).toBe(true);
-        
+
       // });
     }
   }
