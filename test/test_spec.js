7310801a612fdaa1e4f4202727bd612979ee5fcc
Lex Robinson <lexi@lexi.org.uk>
2014-07-12 23:46:20 +0100

    Flesh out the tests

--- test/test_spec.js
+++ test/test_spec.js
@@ -1,29 +1,45 @@
 var labcol = require('../col');
 
-var x = new labcol();
+var step = 15;
 
 describe("col", function() {
-  it("Should actually be a thing", function() {
-    expect(!x).toBe(false);
-  });
+  it("Should not mutate red", function() {
+    var r, g, b, lab, rgb;
+    for (r = 0; r < 255; r += step) {
+      for (g = 0; g < 255; g += step) {
+        for (b = 0; b < 255; b += step) {
+          lab = labcol.RGBtoLab(r, g, b);
+          rgb = labcol.LabtoRGB(lab.l, lab.a, lab.b);
 
-  for (var r = 0; r < 255; r = r + 5) {
-    for (var g = 0; g < 255; g = g + 5) {
-      it("Should actually be a thing", function() {
-        var x = new labcol();
-        var a = x.RGBtoLab(r, g, 0);
-        var b = x.LabtoRGB(a.l, a.a, a.b);
-
-        expect(r == Math.ceil(b.r)).toBe(true);
-      });
-      // it("Should actually be a thing", function () {
-      //   var x = new labcol();
-      //   var a = x.RGBtoLab(r,g,0);
-      //   var b = x.LabtoRGB(a.l,a.a,a.b);
-
-      //   expect(g == Math.floor(b.g)).toBe(true);
-
-      // });
+          expect(Math.ceil(rgb.r)).toEqual(r);
+        }
+      }
     }
-  }
+  });
+  it("Should not mutate green", function() {
+    var r, g, b, lab, rgb;
+    for (r = 0; r < 255; r += step) {
+      for (g = 0; g < 255; g += step) {
+        for (b = 0; b < 255; b += step) {
+          lab = labcol.RGBtoLab(r, g, b);
+          rgb = labcol.LabtoRGB(lab.l, lab.a, lab.b);
+
+          expect(Math.ceil(rgb.g)).toEqual(g);
+        }
+      }
+    }
+  });
+  it("Should not mutate blue", function() {
+    var r, g, b, lab, rgb;
+    for (r = 0; r < 255; r += step) {
+      for (g = 0; g < 255; g += step) {
+        for (b = 0; b < 255; b += step) {
+          lab = labcol.RGBtoLab(r, g, b);
+          rgb = labcol.LabtoRGB(lab.l, lab.a, lab.b);
+
+          expect(Math.ceil(rgb.b)).toEqual(b);
+        }
+      }
+    }
+  });
 });
