392a78df8f34f92a4c931fb499aab107ded4048c
Lex Robinson <lexi@lexi.org.uk>
2014-07-22 21:13:16 +0100

    switch to mocha/chai

--- test/test_spec.js
+++ test/test_spec.js
@@ -1,32 +1,41 @@
-var labcol = require('../col');
+var labcol = require('../col'),
+  chai = require('chai');
+
+var expect = chai.expect;
 
 describe("col", function() {
   describe("basic", function() {
     it("should not mutate red", function() {
       var lab = labcol.RGBtoLab(255, 0, 0);
       var rgb = labcol.LabtoRGB(lab.l, lab.a, lab.b);
-      expect(rgb.r).toEqual(255);
-      expect(rgb.g).toEqual(0);
-      expect(rgb.b).toEqual(0);
+      expect(rgb).to.deep.equal({
+        r: 255,
+        g: 0,
+        b: 0
+      });
     });
     it("should not mutate green", function() {
       var lab = labcol.RGBtoLab(0, 255, 0);
       var rgb = labcol.LabtoRGB(lab.l, lab.a, lab.b);
-      expect(rgb.r).toEqual(0);
-      expect(rgb.g).toEqual(255);
-      expect(rgb.b).toEqual(0);
+      expect(rgb).to.deep.equal({
+        r: 0,
+        g: 255,
+        b: 0
+      });
     });
     it("should not mutate green", function() {
       var lab = labcol.RGBtoLab(0, 0, 255);
       var rgb = labcol.LabtoRGB(lab.l, lab.a, lab.b);
-      expect(rgb.r).toEqual(0);
-      expect(rgb.g).toEqual(0);
-      expect(rgb.b).toEqual(255);
+      expect(rgb).to.deep.equal({
+        r: 0,
+        g: 0,
+        b: 255
+      });
     });
   });
   describe("comprehensive", function() {
     var step = 15;
-    it("Should not mutate red", function() {
+    it("Should not mutate colours", function() {
       var r, g, b, lab, rgb;
       for (r = 0; r < 255; r += step) {
         for (g = 0; g < 255; g += step) {
@@ -34,33 +43,11 @@ describe("col", function() {
             lab = labcol.RGBtoLab(r, g, b);
             rgb = labcol.LabtoRGB(lab.l, lab.a, lab.b);
 
-            expect(Math.ceil(rgb.r)).toEqual(r);
-          }
-        }
-      }
-    });
-    it("Should not mutate green", function() {
-      var r, g, b, lab, rgb;
-      for (r = 0; r < 255; r += step) {
-        for (g = 0; g < 255; g += step) {
-          for (b = 0; b < 255; b += step) {
-            lab = labcol.RGBtoLab(r, g, b);
-            rgb = labcol.LabtoRGB(lab.l, lab.a, lab.b);
-
-            expect(Math.ceil(rgb.g)).toEqual(g);
-          }
-        }
-      }
-    });
-    it("Should not mutate blue", function() {
-      var r, g, b, lab, rgb;
-      for (r = 0; r < 255; r += step) {
-        for (g = 0; g < 255; g += step) {
-          for (b = 0; b < 255; b += step) {
-            lab = labcol.RGBtoLab(r, g, b);
-            rgb = labcol.LabtoRGB(lab.l, lab.a, lab.b);
-
-            expect(Math.ceil(rgb.b)).toEqual(b);
+            expect(rgb).to.deep.equal({
+              r: r,
+              g: g,
+              b: b
+            });
           }
         }
       }
